8608762cd2b898b78683e3fbe5e3eef04f640de4
prateeksha9 <66215313+prateeksha9@users.noreply.github.com>
2021-10-09 01:11:01 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,149 @@
+const display = document.getElementById('clock');
+
+
+// set audio for alarm
+const audio = new Audio('https://assets.mixkit.co/sfx/preview/mixkit-alarm-digital-clock-beep-989.mp3');
+audio.loop = true;
+
+
+let alarmTime = null;
+let alarmTimeout = null;
+
+
+const myList = document.querySelector('#myList');
+const addAlarm = document.querySelector('.setAlarm')
+
+
+// array which will store all the alarms 
+const alarmList = [];
+let count =1;
+
+
+// function to play the alarm at correct time
+function ringing(){
+    audio.play();
+    // remove(now);
+}
+
+
+// function to update time and check the array constantly
+function updateTime() {
+    var today = new Date();
+    const hour = formatTime(today.getHours());
+    const minutes = formatTime(today.getMinutes());
+    const seconds = formatTime(today.getSeconds());
+    const now = `${hour}:${minutes}:${seconds}`;
+
+    display.innerText=`${hour}:${minutes}:${seconds}`;
+    if(alarmList.includes(now) && count === 1){
+        count = count + 1;
+        ringing();
+    } else if( seconds === 59){
+        count = 1;
+    }
+}
+
+
+// set the correct format of time
+function formatTime(time) {
+    if ( time < 10 && time.length != 2) {
+        return '0' + time;
+    }
+    return time;
+}
+
+
+// function to clear alarm
+function clearAlarm() {
+    audio.pause();
+    if (alarmTimeout) {
+        clearTimeout(alarmTimeout);
+        alert('Alarm cleared');
+    }
+}      
+
+
+// event to Delete a particular alarm
+myList.addEventListener('click', e=> {
+    console.log("removing element")
+    if(e.target.classList.contains("deleteAlarm")){
+        e.target.parentElement.remove();
+    }    
+})
+
+
+// remove an alarm from the array
+remove = (value) => {
+    let newList = alarmList.filter((time) => time != value);
+    alarmList.length = 0;                  // Clear contents
+    alarmList.push.apply(alarmList, newList);
+    
+    console.log("newList", newList);
+    console.log("alarmList", alarmList);
+}
+
+
+// display new alarm on webpage
+function showNewAlarm(newAlarm){
+    
+    const html =`
+    <li class = "time-list">        
+        <span class="time">${newAlarm}</span>
+        <button class="deleteAlarm time-control" id="delete-button" onclick = "remove(this.value)" value=${newAlarm}>Delete Alarm</button>       
+    </li>`
+    myList.innerHTML += html
+};
+
+
+// event to set a new alarm when a form is submitted and added to the array
+addAlarm.addEventListener('submit', e=> {
+    e.preventDefault();
+    // const newAlarm = addAlarm.alarmTime.value;
+    let new_h=formatTime(addAlarm.a_hour.value);
+    const new_m=formatTime(addAlarm.a_min.value);
+    const new_s=formatTime(addAlarm.a_sec.value);
+    
+    const newAlarm = `${new_h}:${new_m}:${new_s}`
+
+    if(isNaN(newAlarm)){
+        if(!alarmList.includes(newAlarm)){
+            alarmList.push(newAlarm);
+            console.log(alarmList);
+            console.log(alarmList.length);
+            showNewAlarm(newAlarm);
+            addAlarm.reset();
+        } else{
+            alert(`Alarm for ${newAlarm} already set.`);
+        }
+    } else{
+        alert("Invalid Time Entered")
+    }        
+})
+
+
+// call the function updateTime after a particular interval
+setInterval(updateTime, 1000);
+
+
+
+
+
+
+
+
+// function setAlarm() {
+//     if(alarmTime) {
+//         const current = new Date();
+//         const timeToAlarm = new Date(alarmTime);
+
+//         if (timeToAlarm > current) {
+//             const timeout = timeToAlarm.getTime() - current.getTime();
+//             alarmTimeout = setTimeout(() => audio.play(), timeout);
+//             alert('Alarm set');
+//         }
+//     }
+// }
+
+// function setAlarmTime(value) {
+//     alarmTime = value;
+// }
